40cc6332209da873e43518d28f3c1818b9fc74c7
NumberFortyEight <pavellakhtin@mail.ru>
2021-05-27 16:39:02 +0500

    1.05 edit filemodel

--- src/main/java/com/example/viewer/dataClasses/FileFactory.java
+++ src/main/java/com/example/viewer/dataClasses/FileFactory.java
@@ -7,17 +7,18 @@ import java.io.File;
 
 public class FileFactory {
 
-    public static FileModel createFileModel(Boolean isFolder, String fileName, String pathToRepository, String PathString){
+    public static FileModel createFileModel(Boolean isFolder, String fileName, String pathToRepository,Integer commitTime, String PathString) {
         State state = isFolder ? State.FOLDER : State.FILE;
         String fullPath = PathHelper.getAbsolutePath(pathToRepository) + PathHelper.getAbsolutePath(PathString);
-        return new FileModel(state, fileName, fullPath);
+        return new FileModel(state, fileName, commitTime, fullPath);
     }
 
-    public static FileModel createFileModel(File file, String workPath){
-       return new FileModel.FileModelBuilder()
+    public static FileModel createFileModel(File file, String workPath) {
+        return new FileModel.FileModelBuilder()
                 .name(file.getName())
-                .href( (!workPath.equals("/") ? workPath + "/" + file.getName() : "/" + file.getName()))
+                .href((!workPath.equals("/") ? workPath + "/" + file.getName() : "/" + file.getName()))
                 .state(file.getName().contains(".git") ? State.REPOSITORY : !file.isFile() ? State.FOLDER : State.FILE)
+                .commitTime(0)
                 .build();
     }
 }
